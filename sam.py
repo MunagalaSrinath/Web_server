8e18b3dd3f0ddc20926636bab9d94fa9ee469b0c
MunagalaSrinath <118678482+MunagalaSrinath@users.noreply.github.com>
2023-01-27 01:55:38 +0530

    commit

--- sam.py
+++ sam.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -39,12 +21,3 @@ server_address = ('',80)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-```
-
-## OUTPUT:
-![Screenshot (5)](https://user-images.githubusercontent.com/118344248/214942390-6a6ea06b-65aa-4517-bc85-8fb907d13488.png)
-
-
-
-## RESULT:
-The program executed successfully
